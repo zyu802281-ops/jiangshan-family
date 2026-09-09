df6948fad4ed9a4ea5c0d9fce4c7c8c39ac57c3f
zyu802281-ops <zyu802281@gmail.com>
2026-09-09 12:38:56 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jiangshan-family-v2';
+const CACHE='jiangshan-family-v3';
 const ASSETS=['./','./index.html','./manifest.json','./sw.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
