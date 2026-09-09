f2c3f663d9425cf173062a1fcdf487979c32aa8e
zyu802281-ops <zyu802281@gmail.com>
2026-09-09 12:56:50 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='jiangshan-family-v3';
-const ASSETS=['./','./index.html','./manifest.json','./sw.js'];
+const CACHE='jiangshan-family-v4';
+const ASSETS=['./','./index.html','./manifest.json','./data.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>cached)))});
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
